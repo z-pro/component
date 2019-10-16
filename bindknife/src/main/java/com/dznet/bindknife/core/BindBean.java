8ad63f44ca9adef16e93a4def0835b19aa15ac6e
JasonZeng <380368126@qq.com>
2019-10-16 18:27:40 +0800

    Create BindBean.java

---
+++ bindknife/src/main/java/com/dznet/bindknife/core/BindBean.java
@@ -0,0 +1,15 @@
+package com.dznet.bindknife.core;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BindBean {
+    String alias();
+    String propertyName() default "";
+}
